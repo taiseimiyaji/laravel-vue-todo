4a4b9fb15aabf1b253f093517141ec1d8e086726
Taisei Miyaji <taisei.miyaji@smaregi.jp>
2022-05-12 23:25:09 +0900

    make TaskName ValueObject

---
+++ src/src/Task/ValueObject/TaskName.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Todo\Task\ValueObject;
+
+use InvalidArgumentException;
+use Todo\Shared\Foundation\ValueObject\StringValue;
+
+final class TaskName extends StringValue
+{
+    const MAX_LENGTH = 100;
+
+    public function __construct(string $value)
+    {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    protected function validate(string $value): void
+    {
+        if (mb_strlen($value) === 0) {
+            throw new InvalidArgumentException(sprintf('%s is required.', __CLASS__));
+        }
+        if (mb_strlen($value) > self::MAX_LENGTH) {
+            throw new InvalidArgumentException(sprintf('%s must be %s length.', __CLASS__, self::MAX_LENGTH));
+        }
+    }
+}
