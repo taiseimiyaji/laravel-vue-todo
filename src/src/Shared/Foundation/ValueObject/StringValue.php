8a6c55764a58b28e775279280538dd1386130aec
Taisei Miyaji <taisei.miyaji@smaregi.jp>
2022-05-12 23:24:01 +0900

    prepare test

---
+++ src/src/Shared/Foundation/ValueObject/StringValue.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Todo\Shared\Foundation\ValueObject;
+
+abstract class StringValue
+{
+    /**
+     * Base string value
+     *
+     * @var string
+     */
+    protected string $value;
+
+    /**
+     * Base string constructor
+     *
+     * @param string $value
+     */
+    abstract public function __construct(string $value);
+
+    /**
+     * to string
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return (string)$this->value;
+    }
+
+    /**
+     * string value validate function
+     *
+     * @param string $value
+     * @return void
+     */
+    abstract protected function validate(string $value): void;
+
+    /**
+     * string value judge empty function
+     *
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return mb_strlen($this->value) === 0;
+    }
+}
