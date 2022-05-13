c3542c443152ca5a7c51bce5f81b9b5bd11106cb
Taisei Miyaji <taisei.miyaji@smaregi.jp>
2022-05-13 21:49:38 +0900

    ValueObjectのガワを作成

---
+++ src/src/Shared/Foundation/ValueObject/Identifier.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Todo\Shared\Foundation\ValueObject;
+
+abstract class Identifier
+{
+    abstract protected function validate(): void;
+
+    public function equals(self $id): bool
+    {
+        if (!$id instanceof self) {
+            return false;
+        }
+        return $this->id === $id;
+    }
+}
