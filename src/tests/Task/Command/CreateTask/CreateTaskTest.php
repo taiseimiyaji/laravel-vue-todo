748550bdbbd4753ae2cd50f22a9c630568a4951e
taiseimiyaji <taisei,miyaji4602@gmail.com>
2022-07-09 15:10:27 +0900

    一旦コミット

---
+++ src/tests/Task/Command/CreateTask/CreateTaskTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Task\Command\CreateTask;
+
+use Todo\Task\Command\CreateTask\CreateTask;
+use PHPUnit\Framework\TestCase;
+
+class CreateTaskTest extends TestCase
+{
+    public function test__construct()
+    {
+        
+    }
+}
