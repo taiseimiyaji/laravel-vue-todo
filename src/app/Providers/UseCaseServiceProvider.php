1dbe40e9cf9dc2e247a9f9f525c9d5cefd9fa851
taiseimiyaji <taisei.miyaji4602@gmail.com>
2022-08-02 21:48:40 +0900

    fix: ServiceProviderの修正

--- src/app/Providers/UseCaseServiceProvider.php
+++ src/app/Providers/UseCaseServiceProvider.php
@@ -3,17 +3,17 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Http\Task\Command\CreateTask\CreateTaskAction;
 use Carbon\Laravel\ServiceProvider;
+use Todo\Task\Command\CreateTask\CreateTask;
 use Todo\Task\Command\CreateTask\CreateTaskInterface;
 use Todo\Task\Query\GetAllTask\GetAllTask;
 use Todo\Task\Query\GetAllTask\GetAllTaskInterface;
 
 class UseCaseServiceProvider extends ServiceProvider
 {
-    public function boot(): void{
+    public function boot(): void
+    {
         $this->app->singleton(GetAllTaskInterface::class, GetAllTask::class);
-        $this->app->singleton(CreateTaskInterface::class, CreateTaskAction::class);
-
+        $this->app->singleton(CreateTaskInterface::class, CreateTask::class);
     }
 }
